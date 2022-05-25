8d4e87a3b816d3400972711dd03f9bf5dfdfa392
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-05-25 15:35:50 +0700

    save

--- homework1/frontend/src/App.js
+++ homework1/frontend/src/App.js
@@ -1,7 +1,21 @@
+import { Routes,Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Profile from "./Components/Profile";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+
 
 function App() {
   return (
+
     <>
+    <Navbar/>
+    <Routes>
+          <Route path="/" element={<Profile />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+      </Routes>
     </>
   );
 }
---
+++ homework1/frontend/src/Components/Login.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Button, Checkbox, Form, Input } from 'antd';
+
+export default function Login() {
+  return (
+    <div>Login</div>
+  )
+}
